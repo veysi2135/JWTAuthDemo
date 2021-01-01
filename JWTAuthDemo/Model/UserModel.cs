75213d9bcd3a507dd66ffdb5e756f4b471826bfc
veysi cengiz <weysicengiz@hotmail.com>
2021-01-01 20:46:08 +0300

    user model de değişiklik yapıldı

--- JWTAuthDemo/Model/UserModel.cs
+++ JWTAuthDemo/Model/UserModel.cs
@@ -10,5 +10,6 @@ namespace JWTAuthDemo.Model
         public string UserName { get; set; }
         public string Password { get; set; }
         public string EmailAddress { get; set; }
+
     }
 }
